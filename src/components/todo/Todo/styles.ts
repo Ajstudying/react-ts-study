ce958f06aa13cc874adde022e2fe4fd7ae319c5d
Aejin Kang <dowlsdl003@naver.com>
2023-09-20 17:00:45 +0900

    theme 처리

--- src/components/todo/Todo/styles.ts
+++ src/components/todo/Todo/styles.ts
@@ -9,6 +9,10 @@ div (
 ) 
 */
 export const TodoConatainer = styled.div`
+  // 여기에는 클래스 선택자를 안 쓰는게 낫다.
+  // 다른 곳에서 클래스 이름이 중복될 수 있기 때문
+  // #theme.light(라이트 테마)
+  label: todo-conatainer;
   header {
     input {
       border: 1px solid green;
@@ -42,6 +46,29 @@ export const TodoConatainer = styled.div`
     color: green;
     data {
       font-weight: bold;
+    }
+  }
+
+  //id선택을 먼저 한 것
+  // #app-theme.light
+  // app-theme id가 있는 요소의 클래스가 light인 요소 선택
+  // #app-theme.light 요소 자손인 현재클래스를 선택 -> &
+  //#app-theme.light css-fjslf header input {...} << 실제 스타일 적용
+  #app-theme.light & {
+    header {
+      input {
+        background-color: white;
+        color: black;
+      }
+    }
+  }
+
+  #app-theme.dark & {
+    header {
+      input {
+        background-color: black;
+        color: white;
+      }
     }
   }
 `;
